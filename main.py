e9dd1e7c94e19fdc6e154bd90f281533c3bfbfbe
YoavKehat <yoavkehat@campus.technion.ac.il>
2021-04-14 13:01:06 +0300

    after menachem

--- main.py
+++ main.py
@@ -3,13 +3,14 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import sys
-import panda
+
 
 
 def main(argv):
-    argv[0] = / home / student / C:\Users\kehat\PycharmProjects\hw1_209028869_206016446 / main.py
-    argv[1] = / home / student / C:\Users\kehat\OneDrive\Desktop\HW1 / london.csv
-    argv[2] = "hum, t1, cnt, season, is_holiday"
+    x=4
+    print(x)
+
+
 
 
 # Press the green button in the gutter to run the script.
---
+++ venv/data.py
@@ -0,0 +1,7 @@
+import panda
+
+load_data(path, features)
+    df= pandas.read_csv(path)
+    data = df.to_dict(orient=”list”)
+
+
